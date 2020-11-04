25f13ee73c20926395b285f577d44aede7bd65ca
Jiyoon Park <jiyoonpark0207@gmail.com>
2020-11-04 12:15:19 +0900

    PHP

--- midterms/results.php
+++ midterms/results.php
@@ -2,20 +2,22 @@
 <html>
 
 <head>
-    <title>Bob's Auto Parts - Order
-        Results</title>
+    <title>Bob's Auto Parts - Order Results</title>
 </head>
 
 <body>
     <h1>Bob's Auto Parts</h1>
     <h2>Order Results</h2>
+
     <?php
     echo '<p>Order processed at';
     echo date('H:i, jS F Y');
     echo "</p>";
+
     $tireqty = $_POST['tireqty'];
     $oilqty = $_POST['oilqty'];
     $sparkqty = $_POST['sparkqty'];
+
     echo '<p>Your order is as follows: </p>';
     echo htmlspecialchars($tireqty) . ' tires<br>';
     echo htmlspecialchars($oilqty) . ' bottls of oil<br>';
@@ -25,13 +27,17 @@
     $date =date('H:i, jS F Y');
     $totalqty = 0;
     $totalqty = $tireqty + $oilqty + $sparkqty;
+
     echo "<p>Items ordered: " . $totalqty . "<br>";
+
     $totalamount = 0.00;
     define('TIREPRICE', 100);
     define('OILPRICE', 10);
     define('SPARKPRICE', 4);
+
     $totalamount = $tireqty * TIREPRICE + $oilqty * OILPRICE + $sparkqty * SPARKPRICE;
     echo "Subtotal: $" . number_format($totalamount, 2) . "<br>";
+
     $taxrate = 0.10;
     $totalamount = $totalamount * (1 + $taxrate);
     echo "Total including tax: $" . number_format($totalamount, 2) . "</p>";
@@ -53,6 +59,7 @@
         default:
             break;
     }
+
     $document_root = $_SERVER['DOCUMENT_ROOT'];
     $fp = fopen("$document_root/orders.txt", 'a');
     $_SERVER['DOCUMENT_ROOT'];
@@ -60,8 +67,7 @@
     $output = $date.' '.$tireqty.' tires '.$oilqty.' oil '.$sparkqty.' spark plugs$'.$totalamount.' '.$address.PHP_EOL;
     fwrite($fp, $output, strlen($output));
     fclose($fp);
-
-
+    
     ?>
 </body>
 
---
+++ midterms/week5.js
@@ -0,0 +1,6 @@
+let arr = [1, 2, 3, 4];
+console.log(arr.length);
+arr[6] = 6;
+console.log(arr.length);
+arr.sort();
+console.log(arr);
