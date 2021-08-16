2e6451ff44ed93620e7231e237d036c16ce4b080
jypark <jiyoonpark0207@gmail.com>
2021-08-16 19:29:57 +0900

    finished flask tutorial

--- flask_tutorial/mine/market/__init__.py
+++ flask_tutorial/mine/market/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__) # local python file you are working with
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '4c0f91066c80baf1b608e816'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page" # redirect users to market page
+login_manager.login_message_category = "info"
 from market import routes
--- flask_tutorial/mine/market/models.py
+++ flask_tutorial/mine/market/models.py
@@ -1,7 +1,13 @@
-from market import db
+from market import db, login_manager
 from market import bcrypt
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True) # convention of models
     username = db.Column(db.String(length=30), unique=True, nullable=False)
     email_address = db.Column(db.String(length=50), unique=True, nullable=False)
@@ -10,12 +16,27 @@ class User(db.Model):
     items = db.relationship('Item', backref='owned_user', lazy=True) # allow to see owner of item
 
     @property
+    def prettier_budget(self):
+        if len(str(self.budget)) >= 4:
+            return f'{str(self.budget)[:-3]},{str(self.budget)[-3:]}$'
+        else:
+            return f"{self.budget}$"
+
+    @property
     def password(self):
         return self.password
 
     @password.setter
     def password(self, plain_text_password):
         self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
+    def can_purchase(self, item_obj):
+        return self.budget >= item_obj.price
+    def can_sell(self, item_obj):
+        return item_obj in self.items
 
 
 
@@ -28,3 +49,12 @@ class Item(db.Model):
     owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
     def __repr__(self):
         return f'Item {self.name}'
+    def buy(self, user):
+        self.owner = user.id
+        user.budget -= self.price
+        db.session.commit()
+
+    def sell(self, user):
+        self.owner = None
+        user.budget += self.price
+        db.session.commit()
